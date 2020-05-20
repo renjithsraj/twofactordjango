8f0371282ce5053fe97e743cfd93d8ff6b5f5492
renjith s raj <renjithsraj@live.com>
2020-05-20 16:42:11 +0530

    update django version

--- twofactordjango/settings.py
+++ twofactordjango/settings.py
@@ -79,16 +79,24 @@ WSGI_APPLICATION = 'twofactordjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'test',
+#         'USER': 'renjith',
+#         'PASSWORD': 'wpm',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'test',
-        'USER': 'renjith',
-        'PASSWORD': 'wpm',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'test.db'),
     }
 }
+
 
 # LOGIN_URL = 'two_factor:login'
 #
